b8d1a50f0ddb204534867b05a1bd59d207d8dd28
Lyubo Velikov <lyubo.velikov@chameleontechnology.co.uk>
2020-06-15 16:48:52 +0100

    refactor: Update contact page details and styles.

--- wp-content/themes/avalon/page-contact.php
+++ wp-content/themes/avalon/page-contact.php
@@ -4,13 +4,10 @@
 
 <div class="google-map row">
     <div class="mapouter">
-        <div class="gmap_canvas">
-            <iframe width="100%" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=%D0%B6.%D0%BA.%20%D0%A5%D1%8A%D1%88%D0%BE%D0%B2%D0%B5%2C%20Ruse%2C%20Bulgaria&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-        </div>
+        <div class="gmap_canvas"><iframe width="100%" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=ul.%20%22Panayot%20Hitov%22%2C%20g.k.%20Hashove%2C%20Ruse%2C%20Bulgaria&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div>
         <style>
             .mapouter {
                 position: relative;
-                text-align: right;
                 height: 400px;
                 width: 100%;
             }
@@ -33,18 +30,20 @@
                 </div>
                 <hr>
                 <address class="address">
-                    <strong>Zaza Johnson and Bath</strong><br>
-                    41 St Johns Hill<br>
-                    Shrewsbury<br>
-                    Shropshire<br>
-                    SY1 1JQ
+                    <br>
+                    <strong>Адрес:</strong><br>
+                    Цар Асен 31<br>
+                    Вход 3, Офис 6<br>
+                    Русе<br>
+                    7000
+                    <br><br>
                 </address>
                 <hr>
-                <p>Email:<br>info@zjandb.com</p>
-                <p>Phone:<br><a href="tel:01743 248 351">01743 248 351</a></p>
+                <p>Email:<br>avalon_ds@abv.bg</p>
+                <p>Телефон:<br>082/51-98-51; 0895 606 165;</p>
             </div><!-- /col -->
             <div class="col-lg-6">
-                <div class="officephoto"><img src="https://www.zjandb.com/wp-content/themes/zjandb/assets/images/front-office.jpg" alt=""></div>
+                <div class=""><img class="office-photo" src="https://scontent-lhr8-1.xx.fbcdn.net/v/t1.0-9/32215310_2105045703099392_4711314171474477056_n.jpg?_nc_cat=102&_nc_sid=cdbe9c&_nc_ohc=X9fVqamuHusAX8lso9V&_nc_ht=scontent-lhr8-1.xx&oh=da64296e2e8664cfa2b853c30b00033b&oe=5F0C6759" alt=""></div>
             </div><!-- /col -->
         </div>
     </div>
