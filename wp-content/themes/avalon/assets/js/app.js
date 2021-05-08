a1dc012c0ce181bbdc26fa6a0fbbac3acb284c7d
Lyubomir Velikov <lyubomir@visformatics.net>
2021-05-08 02:19:23 +0100

    feat: Update styles.

--- wp-content/themes/avalon/assets/js/app.js
+++ wp-content/themes/avalon/assets/js/app.js
@@ -14002,59 +14002,58 @@ return jQuery;
 
 var avalon = avalon || {};
 
-avalon.setUpNav = function() {
-    $('.nav li.cat-item:has(ul.children)').addClass('right-arrow');
+avalon.setUpNav = function () {
+  $(".nav li.cat-item:has(ul.children)").addClass("right-arrow");
 };
 
-avalon.setUpCategoryFilter = function() {
-    $('.categories ul ul ul').parent('.cat-item').append('<span class="toggle-cat"><i class="fa fa-caret-down" aria-hidden="true"></i>');
+avalon.setUpCategoryFilter = function () {
+  $(".categories ul ul ul")
+    .parent(".cat-item")
+    .append(
+      '<span class="toggle-cat"><i class="fa fa-caret-down" aria-hidden="true"></i>'
+    );
 
-    $('.toggle-cat').on('click', function(e) {
-        e.preventDefault();
-        $(this).children().toggleClass('fa-caret-down fa-caret-up');
-        $(this).prev().slideToggle('slow');
-        
-    });
-
+  $(".toggle-cat").on("click", function (e) {
+    e.preventDefault();
+    $(this).children().toggleClass("fa-caret-down fa-caret-up");
+    $(this).prev().slideToggle("slow");
+  });
 };
 
+avalon.setSlideshow = function () {
+  $(".slideshow-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: ".slideshow-nav",
+    adaptiveHeight: true,
+  });
 
-avalon.setSlideshow = function() {
-    $('.slideshow-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        fade: true,
-        asNavFor: '.slideshow-nav',
-        adaptiveHeight: false
-    });
-
-    $('.slideshow-nav').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.slideshow-slider',
-        arrows: false,
-        dots: false,
-        centerMode: false,
-        focusOnSelect: true,
-        vertical: false,
-        adaptiveHeight: false
-    });
-
-
+  $(".slideshow-nav").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".slideshow-slider",
+    arrows: false,
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    vertical: false,
+    adaptiveHeight: true,
+  });
 };
 
-$(document).ready(function() {
-    $('body').addClass('visible');
+$(document).ready(function () {
+  $("body").addClass("visible");
 
-    avalon.setUpNav();
-    avalon.setUpCategoryFilter();
-    avalon.setSlideshow();
+  avalon.setUpNav();
+  avalon.setUpCategoryFilter();
+  avalon.setSlideshow();
 
-    lightbox.option({
-        'resizeDuration': 200,
-        'wrapAround': true
-    });
+  lightbox.option({
+    resizeDuration: 200,
+    wrapAround: true,
+  });
 });
 
 //# sourceMappingURL=app.js.map
